d67ed633d875be16c58a500b6dd2f86f82c73312
Kirill Mokhovtsev <kiev.programmer@gmail.com>
2025-01-08 13:39:51 +0000

    Initial commit

---
+++ day00/README.md
@@ -0,0 +1,20 @@
+## Lint
+
+```console
+$ cargo clippy
+```
+
+## Run Tests
+
+(with debug info)
+
+```console
+$ RUST_LOG=debug cargo test -- --nocapture
+```
+
+## Run
+
+```console
+$ export SESSION_COOKIE=session=12345...
+$ cargo run
+```
---
+++ day00/src/main.rs
@@ -0,0 +1,18 @@
+fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+fn main() {
+    println!("Hello, World! {}", add(10, 20));
+}
